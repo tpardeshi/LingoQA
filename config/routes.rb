ac25d8d755d7ff0f3aa99e5e2ebb8dfcb4aa4045
Twinkle Pardeshi <tpardesh@redhat.com>
2019-12-18 12:27:07 +0530

    Remove products and versions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  match ':controller(/:action(/:id))', :via => [:get, :post]
   namespace 'api' do
     namespace 'v1' do
-      resources :screenshots, :only => [:create] do
-        collection do
-          post 'callupload'
-          get 'calldownload'
-        end
-      end
+      post 'screenshots/create', to: 'screenshots#create'
+      resources :screenshots
     end
   end
 end
