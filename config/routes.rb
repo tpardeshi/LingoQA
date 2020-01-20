e1a89dea37d70f0d47c2ac1ef159abb6ed51fa28
Twinkle Pardeshi <tpardesh@redhat.com>
2020-01-20 14:47:02 +0530

    Add html page for image upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   match ':controller(/:action(/:id))', :via => [:get, :post]
   namespace 'api' do
     namespace 'v1' do
-      post 'screenshots/create', to: 'screenshots#create'
+      get 'screenshots/new', to: 'screenshots#new'
+      get 'screenshots/show', to: 'screenshots#show'
       resources :screenshots
     end
   end
