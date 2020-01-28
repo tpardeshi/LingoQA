6f77e055062f6426b017e0aaeb9e647acb1c2265
Twinkle Pardeshi <tpardesh@redhat.com>
2020-01-28 11:49:42 +0530

    Add product API

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  match ':controller(/:action(/:id))', :via => [:get, :post]
+  namespace 'api' do
+    namespace 'v1' do
+      resources :products
+    end
+  end
 end
