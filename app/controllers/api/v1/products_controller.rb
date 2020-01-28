d38d94dea8bfe2dfd39f1eff72644fc7c652c271
Twinkle Pardeshi <tpardesh@redhat.com>
2020-01-28 11:49:42 +0530

    Add product API

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # Controller methods for product model
+    class ProductsController < ApplicationController
+      def index
+        @products = Product.all
+        render json: @products
+      end
+    end
+  end
+end
