5fce15e012de5cb27bba2043bdf5b480d093fd06
Twinkle Pardeshi <tpardesh@redhat.com>
2020-03-04 13:19:50 +0530

    Include validations for all models

--- app/models/version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# The Version class represents the version model
-class Version < ApplicationRecord
-  # Screenshots references a version
-  has_many :screenshots
-  # Version reference a product
-  belongs_to :product
-end
